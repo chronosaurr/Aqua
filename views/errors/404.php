f04202262bec000f669c65d6005aa505b289909b
chronosaurr <roza.dobrysiak@gmail.com>
2025-06-16 16:06:21 +0200

    Dodaj widok błędu 404

---
+++ views/errors/404.php
@@ -0,0 +1,4 @@
+<?php require_once VIEW_PATH . '/partials/header.php'; ?>
+    <h1>Błąd 404 - Nie znaleziono strony</h1>
+    <p>Strona, której szukasz, nie istnieje.</p>
+<?php require_once VIEW_PATH . '/partials/footer.php'; ?>
