acb27d3fc9198f8b16c5e12d311f97114f044a69
chronosaurr <roza.dobrysiak@gmail.com>
2025-06-16 16:53:49 +0200

    Dodaj plik konfiguracyjny config.php z ustawieniami aplikacji i ścieżkami

--- config.php
+++ config.php
@@ -7,18 +7,25 @@ error_reporting(E_ALL);
 
 define('ROOT_PATH', __DIR__);
 define('CONTROLLER_PATH', ROOT_PATH . '/controller');
+define('MODEL_PATH', ROOT_PATH . '/model');
 define('VIEW_PATH', ROOT_PATH . '/views');
-
+define('CORE_PATH', ROOT_PATH . '/core');
 
 define('SITE_NAME', 'Aqua Ticket System');
-
-// Centralna definicja emoji!
 define('EMOJI', [
     'attachment' => '📎',
     'dashboard'  => '📊',
+    'database'   => '📦',
     'department' => '🏢',
     'logout'     => '🚪',
+    'php'        => '🐘',
     'ticket'     => '🎟️',
     'user'       => '👤',
     'users'      => '👥',
 ]);
+
+define('DB_HOST', 'mariadb');             // Nazwa serwisu z docker-compose.yml
+define('DB_NAME', 'system_ticketowy');    // Zgodnie z .env
+define('DB_USER', 'user_ticket');         // Zgodnie z .env
+define('DB_PASS', 'haslo_usera');         // Zgodnie z .env
+define('DB_CHARSET', 'utf8mb4');
